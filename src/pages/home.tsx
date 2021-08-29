a6ade5352348184d54c25a6e1a20a3a62faed28d
chunfen <1070692044@qq.com>
2021-08-29 21:58:21 +0800

    第一个图表

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,11 +8,17 @@ export const Home = () => {
         <div className="home">
             <header style={{backgroundImage:`url(${headerBg})`}}></header>
             <main>
-                <section className="section1"></section>
-                <section className="section2"></section>
-                <section className="section3"></section>
-                <section className="section4"></section>
-                <section className="section5"></section>
+                <section className="section1">
+                    <div className="border fuck">
+                        <h2>派出所管辖统计</h2>
+                        <div className="chart">
+                        </div>
+                    </div>
+                </section>
+                <section className="border section2"></section>
+                <section className="border section3"></section>
+                <section className="border section4"></section>
+                <section className="border section5"></section>
             </main>
         </div>
     );
